1a8c7d5454af5ffad7876e015f417a3a4144d677
David Hillary <145858498+davlary@users.noreply.github.com>
2025-07-13 21:53:58 +0100

    New updates for javascript

--- main.js
+++ main.js
@@ -1,55 +1,107 @@
+// const { json } = require("stream/consumers");
+
 // Get variables
-const itemList = document.querySelector(".item-list");
+const todoList = document.querySelector(".todo-list");
 const filters = document.querySelector(".filters");
 const addTaskBtn = document.querySelector(".add-btn");
 const clearCompleted = document.querySelector(".clear-completed");
 const itemLeft = document.querySelector(".items-left");
 const taskInput = document.querySelector(".task-input");
-const emptyList = document.querySelector(".empty-list")
+const emptyList = document.querySelector(".empty-list");
 
 let todo = [];
 let currentFilter = "all";
 
-addTaskBtn.addEventListener("click", (text) => {
+addTaskBtn.addEventListener("click", () => {
   addTodo(taskInput.value);
 });
 
 taskInput.addEventListener("keydown", (e) => {
-  if (e === "Enter") return addTodo();
-
- 
+  if (e.key === "Enter") addTodo(taskInput.value);
 });
 
-addTodo.addEventListener("change", (text)=>{
-if(text.trim() === "") return;
+function addTodo(text) {
+  if (text.trim() === "") return;
 
+  const todos = {
+    id: Date.now(),
+    text,
+    completed: false,
+  };
 
-const todos = {
-  id: Date.now(),
-  text,
-  completed: false,
-};
+  todo.push(todos);
 
-todo.push(todos);
-addTodo();
-saveTodos();
+  console.log(todos);
+  saveTodos();
 
-console.log(addTodo());
-})
+  renderTodo();
+}
 function saveTodos() {
-  localStorage.setItems("todos", json.Stringify("todos"));
+  localStorage.setItem("todos", JSON.stringify("todos"));
+
+  console.log(saveTodos());
   updateItems();
   emptyItems();
 }
 
-updateItems(){
-    const unCompletedTodo = !todo.completed;
-    itemLeft.textContent = `${unCompletedTodo.length} items ${
-        unCompletedTodo === 1 ? "" : "s"
-    } left`
+function updateItems() {
+  const unCompletedTodo = todos.filter((todo) => !todo.completed);
+  itemLeft.textContent = `${unCompletedTodo.length} items ${
+    unCompletedTodo !== 1 ? "s" : ""
+  } left`;
 }
-emptyItems(){
-    if(unCompletedTodo === 0) emptyList.classList.add("hidden")
+function emptyItems() {
+  // const filteredTodos = filterTodos(currentFilter);
 }
+function filterTodos(filter) {
+  switch (filter) {
+    case "active":
+      return todos.filter((todo) => !todo.completed);
+    case "completed":
+      return todos.filter((todo) => todo.completed);
+    default:
+      return todos;
+  }
+}
+function renderTodo() {
+  todoList.innerHTML = "";
 
-clearCompletedBtn();
+  const filteredTodos = filterTodos(currentFilter);
+  filteredTodos.forEach((todo) => {
+    const todoItem = document.createElement("li");
+    todoItem.classList.add("todo-item");
+    if (todo.completed) todoItem.classList.add("completed");
+
+    const checkBoxContainer = document.createElement(label);
+    checkBoxContainer.classList.add("checkbox-container");
+
+    const checkbox = document.createElement(input);
+    checkbox.type = "checkbox";
+    checkbox.classList.add("todo-checkbox");
+    checkbox.addEventListener("change", () => toggleTodo(todo.id));
+
+    const checkmark = document.createElement("span");
+    checkmark.classList.add("checkmark");
+
+    checkBoxContainer.appendChild(checkbox);
+    checkBoxContainer.appendChild(checkmark);
+
+    const todoText = document.createElement(span);
+    todoText.classList.add("todo-text-input");
+    todoText.textContent = todo.text;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.innerHTML = '<i class="fas fa-times"><i>';
+    deleteBtn.addEventListener("click", deleteTodo(todo.id));
+
+    todoItem.appendChild(todoText);
+    todoItem.appendChild(deleteBtn);
+    todoItem.appendChild(checkBoxContainer);
+
+    todoList.appendChild(todoItem);
+  });
+}
+// function clearCompleted() {}
+function toggleTodo(id) {}
+function deleteTodo(id) {}
